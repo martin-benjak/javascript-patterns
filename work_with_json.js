495c9a3e7bd11e2b4311c91496d3b605bc2b1370
Martin <benjak.martin@gmail.com>
2017-01-21 20:27:23 +0100

    Adding JSON.parse() and other JSON-related knowledge.

--- work_with_json.js
+++ work_with_json.js
@@ -1,3 +1,7 @@
+// JSON object (JSON) is a subset of the object literal notation of JavaScript. In other words, valid JSON is also valid JavaScript object literal notation but not necessarily the other way around. 
+// In context of JavaScript we can see: JSON object, JSON string
+// JSON supports following simple data types: strings, numbers, objects, arrays, Booleans (true or false), null
+
 //  Functions for Working with JSON
 //  JSON.stringify()
 //  Function converts an object to a JSON string
@@ -9,3 +13,11 @@ var obj = {
 };
 
 var s = JSON.stringify(obj);
+
+// JSON.parse()
+// Function converts JSON string to an object
+
+var s2 = '{"first_name" : "Sammy", "last_name" : "Shark", "location" : "Ocean"}',
+	obj2 = JSON.parse(s2);
+
+console.log(obj2.first_name);
